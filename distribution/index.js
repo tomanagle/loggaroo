f4a4e3a7a897688331b9a6c3968368100401b32e
TomNagle <tomanagle@gmail.com>
2019-02-12 08:16:17 +0000

    Update to use TypeScript

--- distribution/index.js
+++ distribution/index.js
@@ -1,35 +1,36 @@
-'use strict';
-
-Object.defineProperty(exports, "__esModule", {
-    value: true
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const moment_1 = __importDefault(require("moment"));
+const winston_1 = __importStar(require("winston"));
+const myFormat = winston_1.format.printf(info => {
+    return `${moment_1.default(info.timestamp).format('DD-MMM-YYYY HH:mm:ss')} ${info.level}: ${String(info.message)}`;
 });
-
-var _moment = require('moment');
-
-var _moment2 = _interopRequireDefault(_moment);
-
-var _winston = require('winston');
-
-var _winston2 = _interopRequireDefault(_winston);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-var myFormat = _winston.format.printf(function (info) {
-    return (0, _moment2.default)(info.timestamp).format('DD-MMM-YYYY HH:mm:ss') + ' ' + info.level + ': ' + String(info.message);
+const logger = winston_1.default.createLogger({
+    transports: [
+        new (winston_1.default.transports.Console)({
+            format: winston_1.format.combine(winston_1.format.colorize(), myFormat)
+        }),
+        new (winston_1.default.transports.File)({
+            level: 'info',
+            filename: 'info.log',
+            format: winston_1.format.combine(winston_1.format.timestamp(), myFormat),
+        }),
+        new (winston_1.default.transports.File)({
+            level: 'error',
+            filename: 'errors.log',
+            format: winston_1.format.combine(winston_1.format.timestamp(), myFormat),
+        })
+    ]
 });
-
-var logger = _winston2.default.createLogger({
-    transports: [new _winston2.default.transports.Console({
-        format: _winston.format.combine(_winston.format.colorize(), myFormat)
-    }), new _winston2.default.transports.File({
-        level: 'info',
-        filename: 'info.log',
-        format: _winston.format.combine(_winston.format.timestamp(), myFormat)
-    }), new _winston2.default.transports.File({
-        level: 'error',
-        filename: 'errors.log',
-        format: _winston.format.combine(_winston.format.timestamp(), myFormat)
-    })]
-});
-
 exports.default = logger;
+//# sourceMappingURL=index.js.map
